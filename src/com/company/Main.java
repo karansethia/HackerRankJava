ab0ae17aa8d8e98d3179f846619fdf93ec3633c6
Karan <karansethia24@gmail.com>
2022-03-05 22:39:39 +0530

    Solving Java Hackerrank Questions

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+/*
+Welcome to the world of Java! In this challenge, we practice printing to stdout.
+
+The code stubs in your editor declare a Solution class and a main method.
+ Complete the main method by copying the two lines of code below and pasting them inside the body of your main method.
+*/
+
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
